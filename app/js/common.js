9d09d4279ad7e8a1d7f5b068f954a618f69fdddb
Valentyn669 <ownage669@gmail.com>
2017-02-10 19:05:51 +0200

    about

--- app/js/common.js
+++ app/js/common.js
@@ -46,3 +46,17 @@ $(function() {
 
 
 });
+
+
+/*counter*/
+$('.count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 4000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
