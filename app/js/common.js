914aca8b800415682909b4d4c89a8fdaa66b9995
Valentyn669 <ownage669@gmail.com>
2017-02-14 16:21:36 +0200

    hotfix

--- app/js/common.js
+++ app/js/common.js
@@ -60,3 +60,22 @@ $('.count').each(function () {
         }
     });
 });
+
+
+$(document).ready(function(){
+//menu
+            var touch = $('#touch-menu');
+            var menu = $('.nav');
+         
+            $(touch).on('click', function(e){
+                e.preventDefault();
+                menu.slideToggle();
+            });
+            $(window).resize(function(){
+                var wid = $(window).width();
+                if(wid > 768 && menu.is(':hidden')){
+                    menu.removeAttr('style');
+                }
+            });
+       
+        });
